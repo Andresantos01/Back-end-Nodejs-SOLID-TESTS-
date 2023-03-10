f2e0709ab832784fd1ebbfeebb158a71ebf8f89e
Andresantos01 <68791372+Andresantos01@users.noreply.github.com>
2023-03-10 02:18:45 -0300

    date-fns

--- src/entities/scheduling.spec.ts
+++ src/entities/scheduling.spec.ts
@@ -1,4 +1,5 @@
 import {expect, test} from 'vitest'
+import { getFutureDate } from '../tests/utils/get-valid-date';
 import { Scheduling } from './scheduling'
 
 test('create an scheduling',  ()=> {
@@ -7,7 +8,7 @@ test('create an scheduling',  ()=> {
 
     startAt.setDate(startAt.getDate() + 1);
     endAt.setDate(endAt.getDate() + 2)
-    
+
     const scheduling = new Scheduling({
         costumer: 'andre',
         startAt,
@@ -20,12 +21,9 @@ test('create an scheduling',  ()=> {
 
 
 test('cannot create an schedule with an invalid end date before startAt', ()=> {
-    const startAt = new Date()
-    const endAt = new Date()
-
-    startAt.setDate(startAt.getDate() + 2);
-    endAt.setDate(endAt.getDate() + 1)
-
+    
+    const startAt = getFutureDate('2022-02-24')
+    const endAt = getFutureDate('2022-02-23')
     expect(()=>{
         return new Scheduling({
             costumer: 'andre',
@@ -37,10 +35,11 @@ test('cannot create an schedule with an invalid end date before startAt', ()=> {
 
 
 test('cannot create an schedule with an invalid startAt before now', ()=> {
+   
     const startAt = new Date()
     const endAt = new Date()
-
-    endAt.setDate(startAt.getDate() - 1)
+    
+    startAt.setDate(startAt.getDate() - 1);
     endAt.setDate(endAt.getDate() + 3)
     expect(()=>{
         return new Scheduling({
