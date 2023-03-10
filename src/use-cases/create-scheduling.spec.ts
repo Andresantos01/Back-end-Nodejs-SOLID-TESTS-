2eb6122729825c4d82e7a0590865fb6c97502ebe
Andresantos01 <68791372+Andresantos01@users.noreply.github.com>
2023-03-10 02:20:40 -0300

    update create scheduling spec

--- src/use-cases/create-scheduling.spec.ts
+++ src/use-cases/create-scheduling.spec.ts
@@ -1,20 +1,46 @@
 import {describe, expect, it} from 'vitest'
 import { Scheduling } from '../entities/scheduling';
+import { getFutureDate } from '../tests/utils/get-valid-date';
 import { CreateSchduling } from './create-scheduling'
-
+import { InMemorySchedulingRepository } from '../in-memory/in-memory-scheduling-repository';
 describe('create scheduling', () => {
     it('should create a schedule',()=>{
-        const createSchduling = new CreateSchduling();
-        const startAt = new Date()
-        const endAt = new Date()
+
+        const startAt = getFutureDate('2022-02-24')
+        const endAt = getFutureDate('2022-02-26')
+
+        const schedulerRepository = new InMemorySchedulingRepository()
+        const createSchduling = new CreateSchduling(schedulerRepository);
         
-        startAt.setDate(startAt.getDate() + 1);
-        endAt.setDate(endAt.getDate() + 2)
+      
 
         expect(createSchduling.execute({
             costumer: 'andre',
             startAt,
             endAt
         })).resolves.toBeInstanceOf(Scheduling)
+    })
+
+
+
+    it('should not create a schedule with overLapping dates', async ()=>{
+        const startAt = getFutureDate('2022-02-20')
+        const endAt = getFutureDate('2022-02-26')
+
+        const schedulerRepository = new InMemorySchedulingRepository()
+        const createSchduling = new CreateSchduling(schedulerRepository);
+        
+       
+
+        await createSchduling.execute({
+            costumer: 'andre',
+            startAt,
+            endAt
+        })
+        expect(createSchduling.execute({
+            costumer: 'andre',
+            startAt : getFutureDate('2022-02-25'),
+            endAt: getFutureDate('2022-02-28')
+        })).rejects.toBeInstanceOf(Error)
     })
 })
