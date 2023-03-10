8bbafafe6be91089b3176c696624f2b1eabea7e8
Andresantos01 <68791372+Andresantos01@users.noreply.github.com>
2023-03-10 02:21:01 -0300

    update scheduling

--- src/use-cases/create-scheduling.ts
+++ src/use-cases/create-scheduling.ts
@@ -1,4 +1,5 @@
 import { Scheduling } from "../entities/scheduling";
+import { SchedulingRepository } from "../repositories/scheduling-repository";
 
 interface CreateSchdulingRequest {
     costumer: string;
@@ -8,16 +9,29 @@ interface CreateSchdulingRequest {
 type CreateSchdulingResponse = Scheduling
 
 export class CreateSchduling {
+
+    constructor(
+        private schedulerRepository: SchedulingRepository
+    ){}
+
     async execute({
         costumer, 
         startAt, 
         endAt
     } : CreateSchdulingRequest) : Promise<CreateSchdulingResponse>{
+
+        const overLappinSchedule = await this.schedulerRepository.findConflictingSchedulings(startAt, endAt)
+        if(overLappinSchedule){
+            throw new Error('Overlappin schedule')
+        }
+
         const scheduling = new Scheduling({
             costumer,
             startAt,
             endAt
         })
+
+        await this.schedulerRepository.create(scheduling)
         return scheduling
     }
 }
